4ca4532735ac72da8e25420d34240351301af394
vncntmchlk <vincentmi@web.de>
2020-05-06 14:57:05 +0200

    finale version noch unaufgerauemt

--- src/sketch_maze_clean.js
+++ src/sketch_maze_clean.js
@@ -1,28 +1,64 @@
 
 // ToDo
-// waere gut zu verhindern dass nicht zweimal der selbe sound gesucht werden muss!
+
+// wenn man falsch liegt und auf nochmal abspielen geht ist irgendwas schief
+
 // bei den levels unterschiedlich viele sounds im labyrinth
 // bisschen aufraumen hier
-// sfs schneiden und evtl neue mit rein
+
+//tobi stuff
+// - Die Sekundendauer des Ausiofiles wird nach Level 1 nicht mehr angezeigt
+// - Ich würde den Punkt mit den Leveln auch verkleiner. Level 5 kann man schon nicht mehr spielen, weil der fest steckt.
+// - einmal wurde eine Musik gespielt, als das nächste Level generiert wurde. Da hatte ich aber auch ganz schnell gespielt und die Anfangsmusik nicht abgewartet.
+// - vielleicht wäre es cool, wenn man in einem späteren Level nicht wieder die Musik suchen muss, die man schonmal gesucht hat. Aber die Wahrscheinligkeit verringert sich ja eh bei mehr Audios
+
 
 let figur; 
 let posFigur = [];
 let clouds = [];
 let wallSize; // wenn die waende zu duenn sind kann man durchglitchen
-let samplePath = [
-  'src/gsg_soundfiles/gsg1.mp3',
-  'src/gsg_soundfiles/gsg2.mp3',
-  'src/gsg_soundfiles/gsg3.mp3',
-  'src/gsg_soundfiles/gsg4.mp3',
-  'src/gsg_soundfiles/gsg5.mp3',
-  'src/gsg_soundfiles/gsg6.mp3',
-  'src/gsg_soundfiles/gsg7.mp3',
-  'src/gsg_soundfiles/gsg8.mp3'
+let samplePathAll = [
+  "src/sf/sample_0.mp3",
+  "src/sf/sample_1.mp3",
+  "src/sf/sample_2.mp3",
+  "src/sf/sample_3.mp3",
+  "src/sf/sample_4.mp3",
+  "src/sf/sample_5.mp3",
+  "src/sf/sample_6.mp3",
+  "src/sf/sample_7.mp3",
+  "src/sf/sample_8.mp3",
+  "src/sf/sample_9.mp3",
+  "src/sf/sample_10.mp3",
+  "src/sf/sample_11.mp3",
+  "src/sf/sample_12.mp3",
+  "src/sf/sample_13.mp3",
+  "src/sf/sample_14.mp3",
+  "src/sf/sample_15.mp3",
+  "src/sf/sample_16.mp3",
+  "src/sf/sample_17.mp3",
+  "src/sf/sample_18.mp3",
+  "src/sf/sample_19.mp3",
+  "src/sf/sample_20.mp3",
+  "src/sf/sample_21.mp3",
+  "src/sf/sample_22.mp3",
+  "src/sf/sample_23.mp3",
+  "src/sf/sample_24.mp3",
+  "src/sf/sample_25.mp3"
+  // 'src/gsg_soundfiles/gsg1.mp3',
+  // 'src/gsg_soundfiles/gsg2.mp3',
+  // 'src/gsg_soundfiles/gsg3.mp3',
+  // 'src/gsg_soundfiles/gsg4.mp3',
+  // 'src/gsg_soundfiles/gsg5.mp3',
+  // 'src/gsg_soundfiles/gsg6.mp3',
+  // 'src/gsg_soundfiles/gsg7.mp3',
+  // 'src/gsg_soundfiles/gsg8.mp3'
   //'src/gsg_soundfiles/gsg9.mp3'
 ];
+let samplePath = [];
 let searchSoundPath;
 let searchSound;
 let searchSoundDuration;
+let lastSearchSound;
 let samples = [];
 let counter = 0;
 let button;
@@ -32,7 +68,7 @@ let nowPlayingFile = undefined;
 let foundText;
 let loadLabText;
 let lives = 1;
-let levelCounter = 1;
+let levelCounter = 4;
 let mazeFreeze = false;
 
 let cols, rows;
@@ -52,48 +88,73 @@ let PlayerStartPos;
 let ballSize;
 let ballW;
 let ballSpeed;
+let startBallSize;
+let allFilesLoaded = 0;
+let allLoaded = false;
+let numOfClouds = 4;
 
 function preload() {
-  searchSoundPath = random(samplePath);
-  // shuffle(samplePath);
-  searchSound = loadSound(searchSoundPath);
-  samplePath.forEach(function(path) {
-    samples.push(loadSound(path));
-  });
+  loadRandomSamples();
 }
 
-// to do random verlorene musik aussuchen und random die musik clouds verteilen
-// button fuer gefunden
+function loadRandomSamples () {
+  let i = 0;
+  samples = [];
+  samplePath = [];
+  while (i < numOfClouds){
+    let newSamp = random(samplePathAll);
+    let containsNewSamp = samplePath.find( e => e === newSamp );
+    if (containsNewSamp == undefined) {
+      samplePath.push(newSamp);
+      samples.push(loadSound(newSamp, countLoadFiles));
+      i++
+    };
+  };
+  searchSoundPath = random(samplePath);
+  while(searchSoundPath == lastSearchSound){
+    searchSoundPath = random(samplePath);
+  };
+  searchSound = loadSound(searchSoundPath);
+}
+
+function countLoadFiles () {
+  allFilesLoaded++
+  if(allFilesLoaded == numOfClouds){
+    allLoaded = true;
+  }
+}
+
 
 function setup() {
     if (fixedSize) { // fuer groesse Displaysv
         useWidth = 800;
         useHeight = 800;
-        startWidth = 70;  
+        startWidth = 100 //70;  
         PlayerStartPos = 30;
         wallSize = 15;
-        ballSize = 24;
-        ballW = 14;
+        startBallSize = 24;
         ballSpeed = 10;
         // mazeOffset = 100;
     } else {
         useWidth = displayWidth;
         useHeight = displayHeight * (7/12);
         // mazeOffset = displayHeight * (1/4);
-        startWidth = useWidth * 0.11;
+        startWidth = useWidth * 0.13;//* 0.11;
         PlayerStartPos = 20;
         wallSize = 12;
-        ballSize = 20;
-        ballW = 12;
+        startBallSize = 20;
         ballSpeed = 9;
     };  
+
+    ballSize = startBallSize;
+    ballW = int(ballSize / 2) + 2;
 
     widthOfWay = startWidth;
     cloudSize = widthOfWay * 0.95;
 
     let elt = document.getElementById('buttonPos');
 
-    buttonFound = createButton('gefunden');
+    buttonFound = createButton('Gefunden');
     buttonFound.mousePressed(checkFound);
     buttonFound.parent(elt);
     buttonFound.hide();
@@ -101,7 +162,7 @@ function setup() {
     button = createButton('verlorene Musik einmal hören');
     button.parent(elt); // use element from page
     button.mousePressed(playSearchSound);
-    searchSoundDuration = searchSound.duration();
+    searchSoundDuration = int(searchSound.duration());
     loadLabText = createP(` Labyrinth Level ${levelCounter} wird generiert ... `);//.addClass('text').hide();
     loadLabText.parent(elt);
     loadLabText.style('color:red;');
@@ -121,6 +182,11 @@ function setup() {
     buttonNext.mousePressed(resetMaze);
     buttonNext.hide();
 
+    loadNewText = createP('Neue Soundfiles laden ...');
+    loadNewText.parent(elt);
+    loadNewText.style('color:white;');
+    loadNewText.hide();
+
     canvas = createCanvas(useWidth, useHeight);
     canvas.parent('sketch-holder');
 
@@ -136,7 +202,7 @@ function setup() {
 function setupClouds (newDim) {
   let numOfCells = newDim[0] * newDim[1];
   let fillArr = Array.from({length: (numOfCells - 2)}, (v,i) => i + 2); // die ersten beiden zellen auslassen
-  for(let i = 0; i < 8; i++){
+  for(let i = 0; i < numOfClouds; i++){
     let indx = floor(fillArr.length * random());
     let removedItem = fillArr.splice(indx,1);
     let cellNum = removedItem[0];
@@ -149,17 +215,21 @@ function setupClouds (newDim) {
 
 function playSearchSound () {
   searchSound.play();
+  searchSoundDuration = int(searchSound.duration());
+  foundText.html(`Wird abgespielt ... (${searchSoundDuration} Sekunden) `);
   foundText.show();
+  button.hide();
   setTimeout(startGame, searchSoundDuration * 1000);
   //startGame();
 }
 
 function startGame () {
-  button.hide();
   buttonFound.show();
   soundPlayed = true;
   mazeFreeze = false;
-  foundText.html(' Die Suche beginnt ...')
+  nowPlayingFile = undefined;
+  foundText.html(' Die Suche beginnt ...');
+  foundText.show();
   setTimeout(weg, 3000);
 }
 
@@ -169,65 +239,107 @@ function weg () {
 
 function checkFound () {
   foundText.show();
+  // console.log(nowPlayingFile);
   if(searchSoundPath == nowPlayingFile){
-    foundText.html(`  gefunden! Level ${levelCounter} geschafft!`);
-    levelCounter ++;
+    if (levelCounter == 5){
+      foundText.html('Gratuliere! Du hast alle Level bewältigt!');
+      levelCounter = 1; // zurueck zu level 1
+      numOfClouds = 4;
+      buttonReset.show();
+    } else {
+      foundText.html(`  Gefunden! Level ${levelCounter} geschafft!`);
+      numOfClouds = 4 + (levelCounter * 2);
+      levelCounter ++;
+      buttonNext.show();
+    };
     mazeFreeze = true;
     buttonFound.hide();
-    buttonNext.show();
     samples.forEach(function(sample) {
       sample.stop();
+      sample.dispose(); // funktioniert ?
     });
+    lastSearchSound = searchSoundPath;
   } else {
     if(nowPlayingFile == undefined){
-      foundText.html(' bewege dich zuerst über eine Klangwolke');
+      foundText.html(' Bewege dich zuerst über eine Klangwolke');
       setTimeout(weg, 3000);
     } else {
       if(lives==1){
-        foundText.html(' das wars noch nicht! 1 Versuch übrig');
+        foundText.html(' Das wars noch nicht! 1 Versuch übrig');
         setTimeout(weg, 3000);
+        lives = 0;
       } else {
-        foundText.html(' leider falsch! Neue Runde?');
+        foundText.html(' Leider falsch! Neue Runde?');
         levelCounter = 1; // zurueck zu level 1
+        numOfClouds = 4;
         mazeFreeze = true;
         buttonFound.hide();
         buttonReset.show();
         samples.forEach(function(sample) {
           sample.stop();
+          sample.dispose(); // funktioniert ?
         });
+        lastSearchSound = searchSoundPath;
       }
-      lives = 1;
     }
   }
 }
 
-function resetMaze () {
+function resetMaze() {
+  allFilesLoaded = 0; 
+  allLoaded = false;
+  buttonFound.hide();
+  buttonReset.hide();
+  buttonNext.hide();
+  loadRandomSamples();
+  if(allLoaded) {
+    resetMazeDo();
+    loadNewText.hide();
+  } else {
+    setTimeout(waitMaze, 250);
+    loadNewText.show();
+  }
+}
+
+function waitMaze() {
+  if(allLoaded) {
+    resetMazeDo();
+    loadNewText.hide();
+  } else {
+    setTimeout(waitMaze, 250);
+  }
+}
+
+function resetMazeDo () {
+  allFilesLoaded = 0; // zuruecksetzen
+  allLoaded = false;
   frameRate(60);
-  searchSoundPath = random(samplePath);
-  searchSound = loadSound(searchSoundPath);
+  // searchSoundPath = random(samplePath);
+  // while(searchSoundPath == lastSearchSound){
+  //   searchSoundPath = random(samplePath);
+  // };
+  // searchSound = loadSound(searchSoundPath);
   grid = [];
   stack = [];
   mazeFinished = false;
-  widthOfWay = int(startWidth - (startWidth * ((levelCounter - 1) / 6)));
+  widthOfWay = int(startWidth - (startWidth * ((levelCounter - 1) / 9)));
   cloudSize = widthOfWay * 0.95;
   let newDim = setupMaze();
   resizeCanvas(newDim[0] * widthOfWay, newDim[1] * widthOfWay); // die haesslichen weissen streifen entfernen
   clouds = [];
   lives = 1;
   setupClouds(newDim);
+  ballSize = int(startBallSize - (startBallSize * ((levelCounter - 1) / 9)));;
+  ballW = int(ballSize / 2) + 2;
   figur.resetPos(PlayerStartPos, PlayerStartPos);
   button.show();
   loadLabText.html(` Labyrinth Level ${levelCounter} wird generiert ... `);
   loadLabText.show();
-  buttonFound.hide();
-  buttonReset.hide();
   foundText.hide();
-  buttonNext.hide();
   soundPlayed = false;
   if (searchSound.isPlaying()) {
     searchSound.pause();
   };
-  // setTimeout(weg, 3000);
 }
 
 function draw() {  
@@ -347,17 +459,18 @@ class SoundCloud {
   onOffSound (){
     this.distanceToFig = dist(this.xpos, this.ypos, posFigur[0], posFigur[1]); //posFigur ist global, da es nur eine gibt
     this.inside = map(this.distanceToFig, this.cloudSize * 0.5, 0, 0, 1);
-    //console.log(this.sample.file);
     if (this.inside < 0) { 
       if (this.sample.isPlaying()) {
         this.sample.pause();
-        nowPlayingFile = undefined;
+        if(nowPlayingFile == this.sample.file){
+          nowPlayingFile = undefined; // die clouds sollen nur ihr eigenes ding zuruecksetzen
+        }
       }
     } else {
       if (this.sample.isPlaying() == false) {
         this.sample.loop();
-        nowPlayingFile = this.sample.file;
       }
+      nowPlayingFile = this.sample.file;
     }
   }
 }
